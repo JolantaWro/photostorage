27b8283d5753f56d50b6b1edf43b2a2847c27be0
JolantaWro <jolanta.woronowska@gmail.com>
2023-09-21 16:00:36 +0200

    added differences between users accounts

--- photo/serializers.py
+++ photo/serializers.py
@@ -1,29 +1,14 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from .models import Photo
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
-
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
-
-
-
 class ImagesSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Photo
         fields = ('__all__')
-        read_only_fields = ('user',)
+        read_only_fields = ('user', 'photo_basic', 'photo_premium', 'photo_active', 'date_added', 'active_until',
+                            'height_basic', 'height_premium', 'time_active')
 
     def create(self, validated_data):
-        # Przypisz zalogowanego użytkownika jako autora zdjęcia
         validated_data['user'] = self.context['request'].user
         return super().create(validated_data)
--- storage/urls.py
+++ storage/urls.py
@@ -20,12 +20,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 from photo import views
 
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers
 
 router = routers.DefaultRouter()
-# router.register(r'users', views.UserViewSet)
-# router.register(r'groups', views.GroupViewSet)
 router.register(r'images', views.ImageViewSet)
 
 
